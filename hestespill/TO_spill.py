b362f1702648b4c3d4c4736a7f845e075b96b521
Hossen Hansen <borzthe95@gmail.com>
2022-10-11 14:50:48 +0200

    jeg har brukt tkinter og jeg er på god vei til å lage det heste gamble spillet

---
+++ hestespill/TO_spill.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import time
+import random
+
+#setter opp main skjerm
+main_screen = Tk()
+main_screen.title('Heste race')
+main_screen.geometry('600x500')
+main_screen.config(background='white')
+
+canvas = Canvas(main_screen,width=600,height=200,bg="black")
+canvas.pack(pady=20)
+
+
+
+
+
+
+
+
+
+
+
+
+main_screen.mainloop()
+
+
